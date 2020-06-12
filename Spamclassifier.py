9ba822d76b028d484ae6281f72c520b925487a77
Arnab Barui <40000635+arnabbarui5@users.noreply.github.com>
2020-06-12 22:34:37 +0530

    Add files via upload

---
+++ Spamclassifier.py
@@ -0,0 +1,87 @@
+# importing the Dataset
+
+import pandas as pd
+
+messages = pd.read_csv('smsspam/SMSSpamCollection', sep='\t',
+                           names=["label", "message"])
+
+#Data cleaning and preprocessing 
+import re
+import nltk
+nltk.download('stopwords')
+
+from nltk.corpus import stopwords
+# from nltk.stem.porter import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+#ps = PorterStemmer()
+lemmatizer = WordNetLemmatizer()
+corpus = []
+for i in range(0, len(messages)):
+    review = re.sub('[^a-zA-Z]', ' ', messages['message'][i])
+    review = review.lower()
+    review = review.split()
+    
+    review = [lemmatizer.lemmatize(word) for word in review if not word in stopwords.words('english')]
+    review = ' '.join(review)
+    corpus.append(review)
+    
+    
+# Creating the Bag of Words model
+#from sklearn.feature_extraction.text import CountVectorizer
+#cv = CountVectorizer(max_features=5000)
+#X = cv.fit_transform(corpus).toarray()
+ 
+# Creating TFIDF Model for better accuracy
+from sklearn.feature_extraction.text import TfidfVectorizer
+cv = TfidfVectorizer()
+X = cv.fit_transform(corpus).toarray()
+
+y=pd.get_dummies(messages['label'])
+y=y.iloc[:,1].values       # 1 for spam and 0 for ham
+
+
+# Train Test Split
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+# Training model using Naive bayes classifier
+
+from sklearn.naive_bayes import MultinomialNB
+spam_detect_model = MultinomialNB().fit(X_train, y_train)
+
+y_pred=spam_detect_model.predict(X_test)
+
+
+from sklearn.metrics import confusion_matrix
+confusion_m = confusion_matrix(y_test, y_pred)
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(y_test, y_pred)
+
+
+# CONCLUSION :
+    
+    #Stemming gives a better accuracy than Lammetization
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
